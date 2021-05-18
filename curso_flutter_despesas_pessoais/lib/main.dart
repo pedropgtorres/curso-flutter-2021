1cb7cdf311e7f34645b3cd0b02cbc84339869f99
Paulo Torres <34400704+pedropgtorres@users.noreply.github.com>
2021-05-18 17:30:32 -0300

    evolução

--- curso_flutter_despesas_pessoais/lib/main.dart
+++ curso_flutter_despesas_pessoais/lib/main.dart
@@ -52,7 +52,49 @@ class MyHomePage extends StatelessWidget {
               children: [
                 ..._transactions.map((tr) {
                   return Card(
-                    child: Text(tr.title),
+                    child: Row(
+                      children: [
+                        Container(
+                          margin: EdgeInsets.symmetric(
+                            horizontal: 15,
+                            vertical: 10,
+                          ),
+                          decoration: BoxDecoration(
+                            border: Border.all(
+                              color: Colors.purple,
+                              width: 2,
+                            ),
+                          ),
+                          padding: EdgeInsets.all(10),
+                          child: Text(
+                            'R\$ ${tr.value.toStringAsFixed(2)}',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                              color: Colors.purple,
+                            ),
+                          ),
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              tr.title,
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 16,
+                              ),
+                            ),
+                            Text(
+                              tr.date.toString(),
+                              style: TextStyle(
+                                color: Colors.grey,
+                              ),
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
                   );
                 }),
               ],
