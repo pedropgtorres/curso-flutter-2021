4e5ab721a17cebc401cd725b59023b655c027088
Paulo Torres <34400704+pedropgtorres@users.noreply.github.com>
2021-05-20 11:04:13 -0300

    evolução

---
+++ curso_flutter_despesas_pessoais/lib/components/transaction_user.dart
@@ -0,0 +1,36 @@
+import 'package:curso_flutter_despesas_pessoais/components/transaction_form.dart';
+import 'package:curso_flutter_despesas_pessoais/components/transaction_list.dart';
+import 'package:curso_flutter_despesas_pessoais/models/transaction.dart';
+import 'package:flutter/material.dart';
+
+class TransactionUser extends StatefulWidget {
+  @override
+  _TransactionUserState createState() => _TransactionUserState();
+}
+
+class _TransactionUserState extends State<TransactionUser> {
+  final _transactions = [
+    Transaction(
+      id: 't1',
+      title: 'Novo tênis corrida',
+      value: 300.76,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Conta de luz',
+      value: 211.30,
+      date: DateTime.now(),
+    ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        TransactionList(_transactions),
+        TransactionForm(),
+      ],
+    );
+  }
+}
--- curso_flutter_despesas_pessoais/lib/main.dart
+++ curso_flutter_despesas_pessoais/lib/main.dart
@@ -1,6 +1,5 @@
-import 'package:curso_flutter_despesas_pessoais/models/transaction.dart';
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
+import 'components/transaction_user.dart';
 
 void main() {
   runApp(MyApp());
@@ -16,24 +15,6 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
-  String title;
-  double value;
-
-  final _transactions = [
-    Transaction(
-      id: 't1',
-      title: 'Novo tênis corrida',
-      value: 300.76,
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: 't2',
-      title: 'Conta de luz',
-      value: 211.30,
-      date: DateTime.now(),
-    ),
-  ];
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -51,90 +32,7 @@ class MyHomePage extends StatelessWidget {
                 elevation: 5,
               ),
             ),
-            Column(
-              children: [
-                ..._transactions.map((tr) {
-                  return Card(
-                    child: Row(
-                      children: [
-                        Container(
-                          margin: EdgeInsets.symmetric(
-                            horizontal: 15,
-                            vertical: 10,
-                          ),
-                          decoration: BoxDecoration(
-                            border: Border.all(
-                              color: Colors.purple,
-                              width: 2,
-                            ),
-                          ),
-                          padding: EdgeInsets.all(10),
-                          child: Text(
-                            'R\$ ${tr.value.toStringAsFixed(2)}',
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 20,
-                              color: Colors.purple,
-                            ),
-                          ),
-                        ),
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              tr.title,
-                              style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 16,
-                              ),
-                            ),
-                            Text(
-                              DateFormat('d MMM y').format(tr.date),
-                              style: TextStyle(
-                                color: Colors.grey,
-                              ),
-                            ),
-                          ],
-                        )
-                      ],
-                    ),
-                  );
-                }),
-              ],
-            ),
-            Card(
-              elevation: 5,
-              child: Padding(
-                padding: EdgeInsets.all(10),
-                child: Column(
-                  children: [
-                    TextField(
-                      onChanged: (newTitle) => title = newTitle,
-                      decoration: InputDecoration(
-                        labelText: 'Título',
-                      ),
-                    ),
-                    TextField(
-                      decoration: InputDecoration(
-                        labelText: 'Valor R\$',
-                      ),
-                    ),
-                    ElevatedButton(
-                      child: Text(
-                        'Nova Transação',
-                        style: TextStyle(
-                          color: Colors.white,
-                        ),
-                      ),
-                      onPressed: () {},
-                      style: ElevatedButton.styleFrom(
-                        primary: Colors.purple,
-                      ),
-                    )
-                  ],
-                ),
-              ),
-            )
+            TransactionUser(),
           ],
         ));
   }
