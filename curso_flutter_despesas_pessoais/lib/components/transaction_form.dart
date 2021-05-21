87f377d7b317ddff6270b046489eef1bd52dc0f4
Paulo Torres <34400704+pedropgtorres@users.noreply.github.com>
2021-05-21 14:25:26 -0300

    listview e teclado num

--- curso_flutter_despesas_pessoais/lib/components/transaction_form.dart
+++ curso_flutter_despesas_pessoais/lib/components/transaction_form.dart
@@ -8,6 +8,17 @@ class TransactionForm extends StatelessWidget {
 
   TransactionForm(this.onSubmit);
 
+  _submitForm() {
+    final title = titleController.text;
+    final value = double.tryParse(valueController.text) ?? 0.0;
+
+    if (title.isEmpty || value <= 0) {
+      return;
+    }
+
+    onSubmit(title, value);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -18,12 +29,15 @@ class TransactionForm extends StatelessWidget {
           children: [
             TextField(
               controller: titleController,
+              onSubmitted: (_) => _submitForm(),
               decoration: InputDecoration(
                 labelText: 'Título',
               ),
             ),
             TextField(
               controller: valueController,
+              keyboardType: TextInputType.number,
+              onSubmitted: (_) => _submitForm(),
               decoration: InputDecoration(
                 labelText: 'Valor R\$',
               ),
@@ -35,11 +49,7 @@ class TransactionForm extends StatelessWidget {
                   color: Colors.white,
                 ),
               ),
-              onPressed: () {
-                final title = titleController.text;
-                final value = double.tryParse(valueController.text) ?? 0.0;
-                onSubmit(title, value);
-              },
+              onPressed: _submitForm,
               style: ElevatedButton.styleFrom(
                 primary: Colors.purple,
               ),
